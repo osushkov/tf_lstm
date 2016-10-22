1fc52a0f785c14f6e6d9d4c390321adf20b5e5ea
Oleg Sushkov <osushkov@gmail.com>
2016-10-22 21:00:39 +1100

    working LSTM network for character level language modeling

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 import numpy as np
 import math
 import textloader
-
+import time
+import sys
 
 def weight(shape):
     stddev = 1.0 / math.sqrt(shape[0])
@@ -13,44 +14,71 @@ def bias(shape):
 
 def sampleBatch(data, steps, batchSize):
     indices = np.arange(steps).reshape([-1, steps]).repeat(batchSize, axis=0)
-    indices += np.random.randint(0, data.shape[1] - steps - 1, (batchSize, 1)).repeat(steps, axis=1)
+    indices += np.random.randint(0, data.shape[0] - steps - 1, (batchSize, 1)).repeat(steps, axis=1)
     return data[indices], data[indices+1]
 
-batchSize = 16
-seqLength = 24
-lstmSize = 32
-numChars = len(textloader.acceptedChars())
+def softmax(x, temperature=1.0):
+    ex = np.exp(x / temperature)
+    return ex / ex.sum()
 
-print("wooo:")
-print(textloader.charVector('c', numChars))
+
+np.random.seed(int(time.time()))
+
+batchSize = 32
+seqLength = 64
+lstmSize = 256
+lstmLayers = 2
+numChars = len(textloader.acceptedChars())
 
 sess = tf.Session()
 
 inputData = tf.placeholder(tf.float32, shape=[batchSize, seqLength, numChars])
 targetData = tf.placeholder(tf.float32, shape=[batchSize, seqLength, numChars])
 
-lstmCell = tf.nn.rnn_cell.BasicLSTMCell(lstmSize)
-initialState = lstmCell.zero_state(batchSize, tf.float32)
+lstmCell = tf.nn.rnn_cell.GRUCell(lstmSize)
+stackedLstm = tf.nn.rnn_cell.MultiRNNCell([lstmCell] * lstmLayers)
 
-lstmOuts, _ = tf.nn.dynamic_rnn(lstmCell, inputData, initial_state=initialState, dtype=tf.float32)
+initialState = stackedLstm.zero_state(batchSize, tf.float32)
+
+embedW = weight([numChars, lstmSize])
+embedB = bias([lstmSize])
+
+W1 = weight([lstmSize, lstmSize])
+B1 = bias([lstmSize])
 
 outputW = weight([lstmSize, numChars])
 outputB = bias([numChars])
 
 probabilities = []
-loss = 0.0
+states = []
 
-for i in range(seqLength):
-    logits = tf.matmul(lstmOuts[:, i, :], outputW) + outputB
-    # probabilities.append(tf.nn.softmax(logits))
-    probabilities.append(logits)
-    loss += tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, targetData[:,i,:]))
+loss = 0.0
+inState = initialState
+
+with tf.variable_scope("RNN"):
+    for i in range(seqLength):
+        if i > 0:
+            tf.get_variable_scope().reuse_variables()
+
+        embedA = tf.nn.elu(tf.matmul(inputData[:, i], embedW) + embedB)
+
+        lstmOut, outState = stackedLstm(embedA, inState)
+
+        preOutput = tf.nn.elu(tf.matmul(lstmOut, W1) + B1)
+        logits = tf.matmul(preOutput, outputW) + outputB
+
+        probabilities.append(logits)
+        states.append(outState)
+
+        inState = outState
+        loss += tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, targetData[:,i,:]))
 
 trainStep =  tf.train.AdamOptimizer().minimize(loss)
 sess.run(tf.initialize_all_variables())
 
 data = textloader.load("data/shakespeare.txt")
-for i in xrange(1000):
+print(data.shape)
+for i in xrange(10000):
     id, td = sampleBatch(data, seqLength, batchSize)
 
     feedDict = {
@@ -62,4 +90,33 @@ for i in xrange(1000):
     print("iter: %d = %f" % (i, loss_value))
 
 
+seed = np.zeros((batchSize, seqLength, numChars))
+seed[0, 0] = textloader.charVector('t', numChars)
+
+state = sess.run([initialState]) # tf.convert_to_tensor(initialState).eval(sess)
+
+for i in xrange(1000):
+    feedDict = {
+        inputData: seed,
+        initialState: state
+    }
+
+    output, nextState = sess.run((probabilities[0], states[0]), feed_dict=feedDict)
+
+    r = np.random.choice(np.arange(output[0].shape[0]), 1, p=softmax(output[0], 0.7))[0]
+    sys.stdout.write(textloader.acceptedChars()[r])
+
+    # print(output[0])
+    # print(nextState)
+    # print(output[0].shape)
+    # print(seed[:,0,:].shape)
+
+    thisChar = np.zeros(numChars)
+    thisChar[r] = 1.0
+
+    seed[0,0] = thisChar
+    state = nextState
+
+sys.stdout.write('\n')
+sys.stdout.flush()
 print("hello world")
